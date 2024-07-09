72b546f2cddc35c575070ed206ea73b0c3566d5d
madhuravani7 <madhubudige67@gmail.com>
2024-07-09 17:13:30 +1000

    home.jsx finished

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Home.scss'
+
+const Home = () => {
+  return (
+    <div className='home-bg'>
+        <section className="home" id="home">
+            <div className="content">
+                <h3>coffee cup</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et architecto porro in aliquam magnam voluptatem?</p>
+                <a href="#about" className='btn'>about us</a>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Home
